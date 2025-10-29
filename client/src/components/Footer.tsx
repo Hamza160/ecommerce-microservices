07ada8318d28fe1266b431690c7243644ef7e83b
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-29 19:52:22 +0500

    feat: Footer Done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  );
+    return (
+        <div className="mt-16 flex flex-col gap-8 items-center md:flex-row md:items-start md:gap-0 bg-gray-800 p-8 rounded justify-between">
+            <div className="flex flex-col gap-4 items-center md:items-start">
+                <Link href="/" className={"flex items-center"}>
+                    <Image
+                        src="/logo.png"
+                        alt="TrendHamza"
+                        width={36}
+                        height={36}
+                        className="w-6 h-6 md:w-9 md:h-9"
+                    />
+                    <p className="hidden md:block text-md font-medium tracking-wider text-white">TRENDHAMZA</p>
+                </Link>
+                <p className="text-sm text-gray-400">@ 2025 TrendHamza.</p>
+                <p className="text-sm text-gray-400">All rights reserved.</p>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Links</p>
+                <Link href="/">Home</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Terms of Service</Link>
+                <Link href="/">Privacy Policy</Link>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Products</p>
+                <Link href="/">All Products</Link>
+                <Link href="/">New Arrivals</Link>
+                <Link href="/">Best Sellers</Link>
+                <Link href="/">Sale</Link>
+            </div>
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Company</p>
+                <Link href="/">About</Link>
+                <Link href="/">Contact</Link>
+                <Link href="/">Blog</Link>
+                <Link href="/">Affiliate Program</Link>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
