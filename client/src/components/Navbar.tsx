2ad75baecfde5751b38f5e628aa2d031451666c7
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-10-29 19:35:09 +0500

    feat: Navbar Completed

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import SearchBar from "@/components/SearchBar";
+import {BellIcon, HomeIcon, ShoppingCart} from "lucide-react";
+
+const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between border-b border-gray-200 pb-4">
+            {/* Left */}
+            <Link href="/" className={"flex items-center"}>
+                <Image
+                    src="/logo.png"
+                    alt="TrendHamza"
+                    width={36}
+                    height={36}
+                    className="w-6 h-6 md:w-9 md:h-9"
+                />
+                <p className="hidden md:block text-md font-medium tracking-wider">TRENDHAMZA</p>
+            </Link>
+            {/* Right */}
+            <div className="flex items-center gap-6">
+                <SearchBar/>
+                <Link href="/">
+                    <HomeIcon className="w-4 h-4 text-gray-600"/>
+                </Link>
+                <BellIcon className="w-4 h-4 text-gray-600"/>
+                <ShoppingCart className="w-4 h-4 text-gray-600"/>
+                <Link href="/login">
+                    Sign in
+                </Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
