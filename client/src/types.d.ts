8435e2611ea90ccaff8b91dbc431c79e80ca852b
Hamza Ashraf <hamzaashraf160@gmail.com>
2025-11-04 01:34:52 +0500

    feat: Cart Page Design Continue

--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -10,3 +10,11 @@ export type ProductType = {
 }
 
 export type ProductsType = ProductType[];
+
+export type CartItemType = ProductType & {
+    quantity: number;
+    selectedColor:string;
+    selectedSize:string;
+}
+
+export type CartItemsType = CartItemType[]
